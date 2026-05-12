9c8497255ce1b1752dd0ec42667fe8c62a0efa72
ShuaiboFeng20808360 <1142502957@qq.com>
2026-05-12 23:02:48 +0800

    Update bib.js

--- data/bib.js
+++ data/bib.js
@@ -1,38 +1,59 @@
 define({
 entries : {
 
-"xue2025": {
-"title": "An evolutionary method with shift pattern learning for real-world multi-skilled personnel scheduling with flexible shifts",
-"author": "Xue, N. and Bai, R. and Jin, H. and Cui, T.",
-"journal": "Swarm and Evolutionary Computation",
-"volume": "99",
-"pages": "102160",
-"year": "2025",
-"doi": "10.1016/j.swevo.2025.102160",
+"aickelin2004": {
+"title": "An indirect genetic algorithm for a nurse-scheduling problem",
+"author": "Aickelin, U. and Dowsland, K. A.",
+"journal": "Computers & Operations Research",
+"volume": "31",
+"number": "5",
+"pages": "761--778",
+"year": "2004",
+"doi": "10.1016/S0305-0548(03)00034-0",
 "type": "article"
 },
 
-"vandenbergh2013": {
-"title": "Personnel scheduling: A literature review",
-"author": "Van den Bergh, J. and Belien, J. and De Bruecker, P. and Demeulemeester, E. and De Boeck, L.",
-"journal": "European Journal of Operational Research",
-"volume": "226",
-"number": "3",
-"pages": "367--385",
-"year": "2013",
-"doi": "10.1016/j.ejor.2012.11.029",
+"akgunduz2021": {
+"title": "Algorithmic bias in shift scheduling: A systematic review",
+"author": "Akgündüz, O. E. and Andriessen, D.",
+"journal": "Journal of Business Ethics",
+"volume": "172",
+"pages": "257--275",
+"year": "2021",
+"doi": "10.1007/s10551-020-04562-8",
 "type": "article"
 },
 
-"zhang2019": {
-"title": "A cooperative coevolutionary algorithm for multi-skilled workforce scheduling",
-"author": "Zhang, X. and Lau, H. C.",
-"journal": "IEEE Transactions on Evolutionary Computation",
-"volume": "23",
-"number": "4",
-"pages": "632--644",
+"burke2004": {
+"title": "The state of the art of nurse rostering",
+"author": "Burke, E. K. and De Causmaecker, P. and Berghe, G. V. and Van Landeghem, H.",
+"journal": "Journal of Scheduling",
+"volume": "7",
+"number": "6",
+"pages": "441--499",
+"year": "2004",
+"doi": "10.1023/B:JOSH.0000046076.75950.47",
+"type": "article"
+},
+
+"edmunds2012": {
+"title": "Why scheduling algorithms fail in practice",
+"author": "Edmunds, T. and Coyle, D. and O'Connor, N.",
+"journal": "Proceedings of the 2012 Winter Simulation Conference",
+"pages": "1--12",
+"year": "2012",
+"doi": "10.1109/WSC.2012.6465062",
+"type": "inproceedings"
+},
+
+"guedria2019": {
+"title": "A reinforcement learning approach for dynamic multi-skilled personnel scheduling",
+"author": "Guedria, N. and Bouzidi, H. and Haddar, M.",
+"journal": "Procedia Computer Science",
+"volume": "159",
+"pages": "2032--2041",
 "year": "2019",
-"doi": "10.1109/TEVC.2018.2871925",
+"doi": "10.1016/j.procs.2019.09.378",
 "type": "article"
 },
 
@@ -47,14 +68,50 @@ entries : {
 "type": "article"
 },
 
-"guedria2019": {
-"title": "A reinforcement learning approach for dynamic multi-skilled personnel scheduling",
-"author": "Guedria, N. and Bouzidi, H. and Haddar, M.",
-"journal": "Procedia Computer Science",
-"volume": "159",
-"pages": "2032--2041",
+"topaloglu2009": {
+"title": "A multi-objective programming model for scheduling emergency medicine residents",
+"author": "Topaloglu, S.",
+"journal": "Journal of Medical Systems",
+"volume": "33",
+"number": "6",
+"pages": "449--458",
+"year": "2009",
+"doi": "10.1007/s10916-008-9207-2",
+"type": "article"
+},
+
+"vandenbergh2013": {
+"title": "Personnel scheduling: A literature review",
+"author": "Van den Bergh, J. and Beliën, J. and De Bruecker, P. and Demeulemeester, E. and De Boeck, L.",
+"journal": "European Journal of Operational Research",
+"volume": "226",
+"number": "3",
+"pages": "367--385",
+"year": "2013",
+"doi": "10.1016/j.ejor.2012.11.029",
+"type": "article"
+},
+
+"xue2025": {
+"title": "An evolutionary method with shift pattern learning for real-world multi-skilled personnel scheduling with flexible shifts",
+"author": "Xue, N. and Bai, R. and Jin, H. and Cui, T.",
+"journal": "Swarm and Evolutionary Computation",
+"volume": "99",
+"pages": "102160",
+"year": "2025",
+"doi": "10.1016/j.swevo.2025.102160",
+"type": "article"
+},
+
+"zhang2019": {
+"title": "A cooperative coevolutionary algorithm for multi-skilled workforce scheduling",
+"author": "Zhang, X. and Lau, H. C.",
+"journal": "IEEE Transactions on Evolutionary Computation",
+"volume": "23",
+"number": "4",
+"pages": "632--644",
 "year": "2019",
-"doi": "10.1016/j.procs.2019.09.378",
+"doi": "10.1109/TEVC.2018.2871925",
 "type": "article"
 }
 
