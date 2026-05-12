2b5137ad1f076d94822087ca7fe8cb9c38c79ebc
ShuaiboFeng20808360 <1142502957@qq.com>
2026-05-12 22:50:57 +0800

    Add files via upload

---
+++ data/bib.js
@@ -0,0 +1,62 @@
+define({
+entries : {
+
+"xue2025": {
+"title": "An evolutionary method with shift pattern learning for real-world multi-skilled personnel scheduling with flexible shifts",
+"author": "Xue, N. and Bai, R. and Jin, H. and Cui, T.",
+"journal": "Swarm and Evolutionary Computation",
+"volume": "99",
+"pages": "102160",
+"year": "2025",
+"doi": "10.1016/j.swevo.2025.102160",
+"type": "article"
+},
+
+"vandenbergh2013": {
+"title": "Personnel scheduling: A literature review",
+"author": "Van den Bergh, J. and Belien, J. and De Bruecker, P. and Demeulemeester, E. and De Boeck, L.",
+"journal": "European Journal of Operational Research",
+"volume": "226",
+"number": "3",
+"pages": "367--385",
+"year": "2013",
+"doi": "10.1016/j.ejor.2012.11.029",
+"type": "article"
+},
+
+"zhang2019": {
+"title": "A cooperative coevolutionary algorithm for multi-skilled workforce scheduling",
+"author": "Zhang, X. and Lau, H. C.",
+"journal": "IEEE Transactions on Evolutionary Computation",
+"volume": "23",
+"number": "4",
+"pages": "632--644",
+"year": "2019",
+"doi": "10.1109/TEVC.2018.2871925",
+"type": "article"
+},
+
+"othman2020": {
+"title": "A memetic algorithm for multi-skilled project scheduling under resource constraints",
+"author": "Othman, M. and Bhuiyan, M.",
+"journal": "Swarm and Evolutionary Computation",
+"volume": "59",
+"pages": "100745",
+"year": "2020",
+"doi": "10.1016/j.swevo.2020.100745",
+"type": "article"
+},
+
+"guedria2019": {
+"title": "A reinforcement learning approach for dynamic multi-skilled personnel scheduling",
+"author": "Guedria, N. and Bouzidi, H. and Haddar, M.",
+"journal": "Procedia Computer Science",
+"volume": "159",
+"pages": "2032--2041",
+"year": "2019",
+"doi": "10.1016/j.procs.2019.09.378",
+"type": "article"
+}
+
+}
+});
